c91d73de540e9c6305864c106ed97d4372df784c
Bridger Tower <bridgertower@gmail.com>
2024-12-03 16:19:13 -0700

    wip

--- src/collections/Books.ts
+++ src/collections/Books.ts
@@ -2,23 +2,30 @@ import { CollectionConfig } from 'payload'
 
 export const Books: CollectionConfig = {
   slug: 'books',
+  access: {
+    read: () => true,
+  },
   fields: [
     {
       name: 'image',
       type: 'upload',
       relationTo: 'media',
+      required: true,
     },
     {
       name: 'title',
       type: 'text',
+      required: true,
     },
     {
       name: 'author',
       type: 'text',
+      required: true,
     },
     {
       name: 'description',
       type: 'text',
+      required: true,
     },
   ],
 }
