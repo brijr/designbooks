9ed36dc2c59cb8965449bfc1761484139cc7863f
Bridger Tower <bridgertower@gmail.com>
2024-12-03 23:16:47 -0700

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import config from '@payload-config'
+import { getPayload } from 'payload'
+
+export default async function Home() {
+  const payload = await getPayload({ config })
+
+  const books = await payload.find({
+    collection: 'books',
+  })
+
   return (
     <div className="p-12">
       <h2>Hello World</h2>
+
+      <ul>
+        {books.docs.map((book) => (
+          <li key={book.id}>
+            <h3>{book.title}</h3>
+            <p>{book.author}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
