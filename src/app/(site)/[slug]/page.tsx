e9e877291b029d7b4ac733ed9d7c58aa453a4c5b
Bridger Tower <bridgertower@gmail.com>
2024-12-04 00:03:37 -0700

    wip

--- src/app/(site)/[slug]/page.tsx
+++ src/app/(site)/[slug]/page.tsx
@@ -2,33 +2,45 @@ import config from '@payload-config'
 import { getPayload } from 'payload'
 import { RichText } from '@payloadcms/richtext-lexical/react'
 
-import { Main } from '@/components/craft'
+import Image from 'next/image'
 
 export default async function Home({ params }: { params: { slug: string } }) {
   const payload = await getPayload({ config })
+  const { slug } = await params
 
   const book = await payload.find({
     collection: 'books',
     where: {
       slug: {
-        equals: params.slug,
+        equals: slug,
+      },
+    },
+  })
+
+  const image = await payload.find({
+    collection: 'media',
+    where: {
+      id: {
+        equals: book.docs[0].image,
       },
     },
   })
 
   return (
-    <Main className="p-12 text-zinc-900">
+    <main className="p-12 text-zinc-900">
       <h2 className="mb-12 font-medium">{book.docs[0].title}</h2>
       <p>by {book.docs[0].author}</p>
       <p>{book.docs[0].description}</p>
 
-      {book.docs[0].summary && (
-        <div className="mt-12">
-          <h3 className="mb-4 font-medium">Summary</h3>
+      {image.docs[0] && (
+        <Image src={image.docs[0].url} alt={image.docs[0].alt} width={500} height={500} />
+      )}
 
+      {book.docs[0].summary && (
+        <div className="mt-12 prose">
           <RichText data={book.docs[0].summary} />
         </div>
       )}
-    </Main>
+    </main>
   )
 }
