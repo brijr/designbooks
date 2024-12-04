d15e5ef7863f755cfe790286efa39d1c26241f77
Bridger Tower <bridgertower@gmail.com>
2024-12-04 10:00:30 -0700

    wip

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -3,12 +3,12 @@ import { notFound } from 'next/navigation'
 import { RichText } from '@payloadcms/richtext-lexical/react'
 
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const revalidate = 600
 
 export default async function Home({ params }: { params: { slug: string } }) {
   const { slug } = await params
-
   const book = await queryBookBySlug({ slug })
 
   if (!book) {
@@ -16,20 +16,27 @@ export default async function Home({ params }: { params: { slug: string } }) {
   }
 
   return (
-    <main className="p-12 text-zinc-900">
-      <h2 className="mb-12 font-medium">{book.title}</h2>
-      <p>by {book.author}</p>
-      <p>{book.description}</p>
+    <main className="p-12 text-zinc-900 grid gap-12">
+      <h2 className="font-medium">
+        <Link href="/">A Collection of Books on Design</Link>
+      </h2>
+
+      <div>
+        <h2 className="font-medium">{book.title}</h2>
+        <p>by {book.author}</p>
+        <p>{book.description}</p>
+      </div>
 
       <Image
-        src={book.image.url || './placeholder.jpg'}
+        className="w-36 h-fit"
+        src={book.image.url}
         alt={book.image.alt}
-        width={book.image.width || 600}
-        height={book.image.height || 800}
+        width={book.image.width}
+        height={book.image.height}
       />
 
       {book.summary && (
-        <div className="mt-12 prose">
+        <div className="prose">
           <RichText data={book.summary} />
         </div>
       )}
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,26 +1,31 @@
-import Link from 'next/link'
 import { queryAllBooks } from '@/lib/data'
+
+import Link from 'next/link'
 
 export default async function Home() {
   const books = await queryAllBooks()
 
   return (
-    <main className="p-12 text-zinc-900">
-      <h2 className="mb-12 font-medium">Hello World</h2>
+    <main className="p-12 text-zinc-900 grid gap-12">
+      <h2 className="font-medium">
+        <Link href="/">A Collection of Books on Design</Link>
+      </h2>
 
-      <ul>
-        {books.map((book) => (
-          <Link
-            href={`/${book.slug}`}
-            key={book.id}
-            className="block transition-all hover:pl-1 hover:-pr-1"
-          >
-            <h3>
-              {book.title} <span className="text-zinc-400 ml-1">{book.author}</span>
-            </h3>
-          </Link>
-        ))}
-      </ul>
+      {books.length > 0 && (
+        <ul>
+          {books.map((book) => (
+            <Link
+              href={`/${book.slug}`}
+              key={book.id}
+              className="block transition-all hover:pl-1 hover:-pr-1"
+            >
+              <h3>
+                {book.title} <span className="text-zinc-400 ml-1">{book.author}</span>
+              </h3>
+            </Link>
+          ))}
+        </ul>
+      )}
     </main>
   )
 }
