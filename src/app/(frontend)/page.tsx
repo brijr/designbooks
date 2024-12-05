08aa84fd11eeee0372edac1eccd5dea6437a820c
Bridger Tower <bridgertower@gmail.com>
2024-12-05 11:53:46 -0700

    wip

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,6 @@
 import { queryAllBooks } from '@/lib/data'
 import { Book } from '@/payload-types'
+import Image from 'next/image'
 
 import Link from 'next/link'
 
@@ -24,12 +25,15 @@ export default async function Home() {
 }
 
 const BookCard = ({ book }: { book: Book }) => {
+  const cover: any = book?.image
+
   return (
     <Link
       href={`/${book.slug}`}
       key={book.id}
-      className="block transition-all hover:pl-1 hover:-pr-1"
+      className="p-8 flex items-center justify-center aspect-square bg-zinc-200 rounded-lg"
     >
+      <Image src={cover.url} width={cover.width} height={cover.height} alt={cover.alt} />
       <h3>
         {book.title} <span className="text-zinc-400 ml-1">{book.author}</span>
       </h3>
